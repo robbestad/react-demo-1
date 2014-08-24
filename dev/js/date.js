6ac3d2d349520d76c510a612060d77c7b5dbc46a
Sven Anders Robbestad <robbestad@gmail.com>
2014-08-24 14:54:58 +0200

    less jquery

--- dev/js/date.js
+++ dev/js/date.js
@@ -1,24 +1,45 @@
 /**
  * @jsx React.DOM
  */
-var DateApplication = React.createClass({displayName: 'DateApplication',
+var Masthead = React.createClass({displayName: 'Masthead',
     render: function() {
-        var elapsed = Math.round(this.props.elapsed  / 100);
-        var seconds = elapsed / 10 + (elapsed % 10 ? '' : '.0' );
-        var message =
-            'React is UP & has been successfully running for ' + seconds + ' seconds.';
+        var divStyle= {
+            position: 'absolute',
+            top: this.props.scrollTop,
+            left: '0',
+            width: '100%',
+            height: '270px',
+            background: 'url(img/bg.jpg) no-repeat 50% 50%',
+            backgroundSize:'cover',
+            transform: 'translateZ(0) scale(1)',
+            zIndex: '-1',
+            textAlign: 'center'
+        }
 
-        return React.DOM.p(null, message);
+        var logoStyle = {
+            //width: '100%',
+            top: this.props.logoTop,
+            //margin: '50px auto',
+            color: 'black',
+            width: '150px',
+            fontSize: '5em',
+            left: (this.props.width/2)-110+"px",
+            position: 'absolute'
+        }
+
+        return  React.DOM.div({style: divStyle}, 
+             React.DOM.h1({style: logoStyle}, "React")
+            );
     }
+
+
 });
-var start = new Date().getTime();
+
 setInterval(function() {
-
-    var seconds =new Date().getTime() - start;
-    $(".masthead").css("top",-$(window).scrollTop()/2+"px");
-
+    var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+    var width=document.body.clientWidth;
     React.renderComponent(
-        DateApplication({elapsed: seconds}),
-        document.getElementById('container')
+        Masthead({width: width, scrollTop: -scrollTop, logoTop: $(window).scrollTop()/1.75}),
+        document.getElementById('masthead')
     );
 }, 0);
